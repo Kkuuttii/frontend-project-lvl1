f7b5af77995b99ff2ed37e53283819d6c1dee6b6
Kkuuttii <katerinaugolek@gmail.com>
2022-09-01 22:08:56 +0300

    ШАГ 6, создала файл с общей для всех игр логикой.

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import readlineSync from 'readline-sync';
+import greeting from './cli.js';
+
+const introduction = (gameСonditions) => {
+  console.log('Welcome to the Brain Games!!!!');
+  const userName = greeting();
+  console.log(`Hello, ${userName}!`);
+
+  if (gameСonditions !== undefined) {
+    console.log(gameСonditions);
+  }
+  return userName;
+};
+
+export const question = (interrogation) => {
+  console.log(`Question: ${interrogation}`);
+};
+
+export const answer = () => {
+  const userAnswer = readlineSync.question('Your answer: ');
+  return userAnswer;
+};
+
+export const randomInteger = (numberSize = 2, decimalPointNumber = 2) => {
+  const size = 10 ** numberSize;
+  const randomNunber = Math.random();
+  const integer = randomNunber.toFixed(decimalPointNumber) * size;
+  return integer.toFixed(0);
+};
+
+export const wrongAnswerWarning = (userAnswer, oppositeAnswer, userName) => {
+  console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${oppositeAnswer}.`);
+  console.log(`Let's try again, ${userName}`);
+};
+
+export const winAnnouncement = (counter, numberOfRounds, userName) => {
+  if (counter === numberOfRounds) {
+    console.log(`Congratulations, ${userName}`);
+  }
+};
+
+export default introduction;
